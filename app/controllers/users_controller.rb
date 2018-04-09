a9111c1e305333bbb35c93859cf6c141fc466720
Ayaka Takehara <ayaaa9696@gmail.com>
2018-04-09 17:27:53 +0900

    create page foo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,10 @@ class UsersController < ApplicationController
     end
   end
 
+  # FOO /foo
+  def foo
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
